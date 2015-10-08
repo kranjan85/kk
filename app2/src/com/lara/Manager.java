3ec270d9e47b915ca6721a7cb0004a9e6c376408
Kaushik <kranjan786@gmail.com>
2015-10-08 17:26:41 +0530

    Added again

---
+++ app2/src/com/lara/Manager.java
@@ -0,0 +1,34 @@
+package com.lara;
+public class Manager 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println("Hello Kaushik What Are You Doing?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+		System.out.println(" Are You Studying?");
+	}
+}
